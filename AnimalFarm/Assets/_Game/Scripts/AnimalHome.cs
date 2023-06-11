9c6f0398731590b6e7d898a183454a74ba4d7db2
Oknaa <mohammadlaadidaoui@gmail.com>
2023-06-11 16:12:01 +0100

    Fixed the cow sprite

--- AnimalFarm/Assets/_Game/Scripts/AnimalHome.cs
+++ AnimalFarm/Assets/_Game/Scripts/AnimalHome.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Game.Scripts;
 using UnityEngine;
 
-public class AnimalHome : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class AnimalHome : MonoBehaviour {
+    public AnimalType animalType;
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (!col.CompareTag("Player")) return;
+
+        if (col.GetComponent<Animal>().animalType == animalType)
+            OnCorrectAnimalTrigger();
+        else
+            OnWrongAnimalTrigger();
+
+        Destroy(col.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnWrongAnimalTrigger() {
+        ScoreSystem.Instance.RemoveScore(1);
+        print("Wrong Animal");
+    }
+
+    private void OnCorrectAnimalTrigger() {
+        ScoreSystem.Instance.AddScore(1);
+        print("Correct Animal");
     }
 }
