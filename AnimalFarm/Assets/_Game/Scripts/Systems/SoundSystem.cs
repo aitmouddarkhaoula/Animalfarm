00d4fac161da6c4674774afee7bf4577e7be4a9a
Oknaa <mohammadlaadidaoui@gmail.com>
2023-06-12 21:42:40 +0100

    Imported SFXs

---
+++ AnimalFarm/Assets/_Game/Scripts/Systems/SoundSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OknaaEXTENSIONS.CustomWrappers;
+using UnityEngine;
+
+public class SoundSystem : Singleton<SoundSystem> {
+    private AudioSource audioSource;
+    
+    public AudioClip animalClickSound;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAnimalClickSound(AnimalType animalType) {
+        audioSource.PlayOneShot(animalClickSound);
+    
+        
+    }
+}
